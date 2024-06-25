56793c99df40acdefcfc03e28a8a33ff0acd86fa
simran <simrankumaridga@gmail.com>
2024-06-25 13:15:29 +0530

    menu item send to admin

--- app/Http/Controllers/api/AdminAuthController.php
+++ app/Http/Controllers/api/AdminAuthController.php
@@ -72,6 +72,127 @@ class AdminAuthController extends Controller
             $imagePath = url('/Admin/' . $user->image);
             $code = 200;
 
+                $menuList = [
+                [
+                    'title' => 'Dashboard',
+                    'iconStyle' => ' <i className="material-symbols-outlined">home</i>',
+                    'to' => 'dashboard',
+                ],
+                [
+                    'title' => 'Student',
+                    'classsChange'=> 'mm-collapse',
+                    'iconStyle' => '<i className="material-symbols-outlined">school</i>',
+                            ' content'=> [
+                        [
+                            'title'=> 'Student',
+                            'to'=> 'student',					
+                        ],
+                        [
+                            'title'=> 'Student Detail',
+                            'to'=> 'student-detail',
+                        ],
+                        [
+                            'title'=> 'Add New Student',
+                            'to'=> 'add-student',
+                        ],
+           
+                        ],
+                ],
+                [
+                    'title' => 'Teacher',
+                    'classsChange' => 'mm-collapse',	
+                    'iconStyle' => '<i className="material-symbols-outlined">person</i>',
+                        ' content'=> [
+                        [
+                            'title'=> 'Teacher',
+                            'to'=> 'teacher',					
+                        ],
+                        [
+                            'title'=> 'Teacher Detail',
+                            'to'=> 'teacher-detail',
+                        ],
+                        [
+                            'title'=> 'Add New Teacher',
+                            'to'=> 'add-teacher',
+                        ],
+           
+                        ],
+                ],
+                [
+                    'title' => 'Courses (Batch)',
+                    'classsChange' => 'mm-collapse',
+                    'iconStyle' => '<i className="merial-icons">article</i>',
+                                            ' content'=> [
+                        [
+                            'title'=> 'Batch',
+                            'to'=> 'batch',					
+                        ],
+                        [
+                            'title'=> 'Add New Batch',
+                            'to'=> 'add-batch',
+                        ],
+                      
+           
+                        ],
+                ],
+                [
+                    'title' => 'Staff',
+                     'classsChange' => 'mm-collapse',
+                     'iconStyle' => '<i className="merial-icons">article</i>',
+                        ' content'=> [
+                        [
+                            'title'=> 'Staff',
+                            'to'=> 'staff',					
+                        ],
+                        [
+                            'title'=> 'Add New staff',
+                            'to'=> 'add-staff',
+                        ],
+                      
+           
+                        ],
+                ],
+                [
+                    'title' => 'Reports',
+                     'classsChange' => 'mm-collapse',
+                     'iconStyle' => '<i className="merial-icons">article</i>',
+                        ' content'=> [
+                        [
+                            'title'=> 'Live Classes',
+                            'to'=> 'live-classes',					
+                        ],
+                        [
+                            'title'=> 'Create Live Class',
+                            'to'=> 'page-lock-screen',
+                        ],
+                      
+           
+                        ],
+                ],
+                [
+                    'title' => 'Attendance',
+                     'classsChange' => 'mm-collapse',
+                     'iconStyle' => '<i className="merial-icons">article</i>',
+                        ' content'=> [
+                        [
+                            'title'=> 'Attendance',
+                            'to'=> 'page-lock-screen',					
+                        ],
+                        [
+                            'title'=> 'Todays Attendance',
+                            'to'=> 'page-lock-screen',
+                        ],
+                      
+           
+                        ],
+                ],
+                [
+                    'title' => 'Settings',
+                    'iconStyle' => '<i className="material-icons">settings</i>',
+                    'to' => 'settings',
+                ],
+            ];
+
             $data = [
             'admin' => [
             'id' => $user->id,
@@ -81,7 +202,8 @@ class AdminAuthController extends Controller
             'image' => $imagePath, // Include the full image URL
             ],
                 'token' => $token,
-                 'message' => 'Login Successfully'
+                 'message' => 'Login Successfully',
+                  'menuList' => $menuList
             ];
         }
     } catch (Exception $e) {
