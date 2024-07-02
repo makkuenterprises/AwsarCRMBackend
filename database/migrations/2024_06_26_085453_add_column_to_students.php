37c0925d95667544069911a83097f77f976dc176
simran <simrankumaridga@gmail.com>
2024-07-02 13:40:57 +0530

    updated

--- database/migrations/2024_06_26_085453_add_column_to_students.php
+++ database/migrations/2024_06_26_085453_add_column_to_students.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('students', function (Blueprint $table) {
 
-            $table->string('course_id')->nullable();
+            // $table->string('course_id')->nullable();
 
         });
     }
