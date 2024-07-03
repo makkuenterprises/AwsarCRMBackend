9e59a118b20aee5b0abbffbfaa5bb56d48f37456
simran <simrankumaridga@gmail.com>
2024-07-03 12:44:53 +0530

    updated

--- database/migrations/2024_06_26_063917_add_column_to_courses_enrollements.php
+++ database/migrations/2024_06_26_063917_add_column_to_courses_enrollements.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('courses_enrollements', function (Blueprint $table) {
             //
-            $table->string('paid_amount')->nullable();
+            // $table->string('paid_amount')->nullable();
 
         });
     }
--- database/migrations/2024_07_03_071159_add_column_to_courses_enrollements.php
+++ database/migrations/2024_07_03_071159_add_column_to_courses_enrollements.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::table('courses_enrollements', function (Blueprint $table) {
             //
-            // $table->string('payment_status')->nullable();
+            $table->string('payment_status')->nullable();
+            $table->string('paid_amount')->nullable();
+
 
         });
     }
