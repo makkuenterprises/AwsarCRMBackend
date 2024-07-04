a80f38ae59d0d4aca7e2ebd27654cc21318363e8
simran <simrankumaridga@gmail.com>
2024-07-04 13:10:46 +0530

    updated

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
+     protected $fillable = [
+        'student_id', 'date', 'status', 'course_id', // Add 'student_id' here
+    ];
 }
