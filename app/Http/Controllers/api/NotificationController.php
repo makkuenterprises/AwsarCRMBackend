0d1168721a6a0d7bf789b2927f2fec15f2279d24
simran <simrankumaridga@gmail.com>
2024-07-08 10:55:44 +0530

    updated

--- app/Http/Controllers/api/NotificationController.php
+++ app/Http/Controllers/api/NotificationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Notification;
+use DB;
 
 
 class NotificationController extends Controller
