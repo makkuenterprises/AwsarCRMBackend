d81eac84fdf8fb7df0ea83f0291ae6db9eba7041
simran <simrankumaridga@gmail.com>
2024-07-08 10:20:23 +0530

    batch added in notice

--- app/Http/Controllers/api/NotificationController.php
+++ app/Http/Controllers/api/NotificationController.php
@@ -10,34 +10,39 @@ use App\Models\Notification;
 
 class NotificationController extends Controller
 {
-    public function create(Request $request){
 
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|string|min:1|max:255',
-            'description' => ['required', 'string', 'min:1', 'max:250'],
-            'sendTo' => ['required', 'string', 'min:1', 'max:250'],
-        ]);
-        if ($validator->fails()) {
-            return response()->json([
-             'status' => false,
-               'code'=>400,
-              'errors' => $validator->errors()
-              ], 400);
-        }
-        try{
-            $notification = new Notification();
-            $notification->title = $request->input('title');
-            $notification->description = $request->input('description');
-            $notification->sendTo = $request->input('sendTo');
-            $notification->save();
-            return response()->json(['status'=>true,'code'=>200,'message' => 'Notification created successfully', 'notification' => $notification], 200);
-            }catch (Exception $e) {
-            $data = ['error' => $e->getMessage()];
-            return response()->json(['status'=>false,'code'=>500,'message' => 'An error occurred while Created Notification', 'data' => $data], 500);
-        }
-         
+public function create(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|string|min:1|max:255',
+        'description' => ['required', 'string', 'min:1', 'max:250'],
+        'sendTo' => ['required', 'string', 'min:1', 'max:250'],
+        'batch' => 'required|integer' // Add validation rule for batch
+    ]);
 
+    if ($validator->fails()) {
+        return response()->json([
+            'status' => false,
+            'code' => 400,
+            'errors' => $validator->errors()
+        ], 400);
     }
+
+    try {
+        $notification = new Notification();
+        $notification->title = $request->input('title');
+        $notification->description = $request->input('description');
+        $notification->sendTo = $request->input('sendTo');
+        $notification->batch = $request->input('batch'); // Set the batch value
+        $notification->save();
+
+        return response()->json(['status' => true, 'code' => 200, 'message' => 'Notification created successfully', 'notification' => $notification], 200);
+    } catch (Exception $e) {
+        $data = ['error' => $e->getMessage()];
+        return response()->json(['status' => false, 'code' => 500, 'message' => 'An error occurred while creating notification', 'data' => $data], 500);
+    }
+}
+ 
 
     public function list(){
 
