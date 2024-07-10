929e5d93ed9b6c946a593686bcfe369a9c4456e7
simran <simrankumaridga@gmail.com>
2024-07-10 11:06:38 +0530

    updated

--- app/Models/PaymentGateway.php
+++ app/Models/PaymentGateway.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentGateway extends Model
 {
     use HasFactory;
+       protected $fillable = [
+        'name',
+        'api_key',
+        'api_secret',
+        'description',
+     
+    ];
 }
