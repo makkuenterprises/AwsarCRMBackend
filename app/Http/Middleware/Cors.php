2e2fc74650d1eedf414c0d9241b6fe2d45c7c29c
simran <simrankumaridga@gmail.com>
2024-07-11 15:22:23 +0530

    updated

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -35,6 +35,10 @@ class Cors
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type');
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self'; object-src 'none';");
+        $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
 
         return $response;
     }
