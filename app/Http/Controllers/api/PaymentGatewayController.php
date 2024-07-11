d809d84b42a5e51169e2c641c741dfa4dbe2f494
simran <simrankumaridga@gmail.com>
2024-07-11 11:45:11 +0530

    updated

--- app/Http/Controllers/api/PaymentGatewayController.php
+++ app/Http/Controllers/api/PaymentGatewayController.php
@@ -73,10 +73,10 @@ class PaymentGatewayController extends Controller
             $coursesCount = Course::count();
 
             $partialPaymentStudentsCount = Student::where('payment_status', 'partial')->count();
-            $notEnrollStudentsCount = Student::where('course', 'Not Enrolled')->count();
+            $notEnrollStudentsCount = Student::where('course_id', 'Not Enrolled')->count();
             $fullPaymentStudentsCount = Student::where('payment_status', 'full')->count();
 
-            return response()->json([
+            return response()->json([ 
                 'success' => true,
                 'data' => [
                     'teachersCount' => $teachersCount,
