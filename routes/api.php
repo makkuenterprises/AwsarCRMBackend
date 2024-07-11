988484ca28d50b37b3cc6caf39a0822bc4f37f7b
simran <simrankumaridga@gmail.com>
2024-07-11 11:02:38 +0530

    updated

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\api\DetailsController;
 use App\Http\Controllers\api\LeaveRequestController;
 use App\Http\Controllers\api\ClassRoutineController;
 use App\Http\Controllers\api\PaymentGatewayController;
-// use App\Http\Controllers\api\DashboardData;
+use App\Http\Controllers\api\DashboardData;
 
 
 
