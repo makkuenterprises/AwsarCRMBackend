28444d1a0bf2822da403cbd61b3f23cf9252da35
simran <simrankumaridga@gmail.com>
2024-07-20 11:21:22 +0530

    updated

--- database/migrations/2024_07_20_050455_create_exams_table.php
+++ database/migrations/2024_07_20_050455_create_exams_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
         Schema::create('exams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->timestamp('start_time');
-            $table->timestamp('end_time');
+            $table->string('start_time');
+            $table->string('end_time');
              $table->unsignedBigInteger('batch_id'); 
             $table->float('passing_marks');
             $table->timestamps();
 
-               $table->foreign('batch_id')->references('id')->on('batches')->onDelete('cascade');
+               $table->foreign('batch_id')->references('id')->on('courses')->onDelete('cascade');
         });
     }
 
