447aec8b3a8e5c0a95cf9cb33a1f5dcc6133cc63
simran <simrankumaridga@gmail.com>
2024-07-25 11:57:05 +0530

    updated

--- app/Models/SlidesImages.php
+++ app/Models/SlidesImages.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class SlidesImages extends Model
 {
     use HasFactory;
+      // Define the table associated with the model
+    protected $table = 'slides_images'; 
+
+    // Specify the fillable fields
+    protected $fillable = [
+        'path',
+        'title',
+        'link',
+    ];
+
 }
