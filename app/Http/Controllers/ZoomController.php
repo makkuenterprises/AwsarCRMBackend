bc93bccd40f29d0ee28ac4b767e925092859570b
simran <simrankumaridga@gmail.com>
2024-08-03 11:31:51 +0530

    updated

--- app/Http/Controllers/ZoomController.php
+++ app/Http/Controllers/ZoomController.php
@@ -141,7 +141,7 @@ public function createMeeting(Request $request)
 
 private function create_a_zoom_meeting($meetingConfig, $accessToken)
 {
-    dd($$meetingConfig);
+    dd($meetingConfig);
     $requestBody = [
         'topic'      => $meetingConfig['topic'] ?? 'New Meeting',
         'type'       => 2, // Scheduled meeting
