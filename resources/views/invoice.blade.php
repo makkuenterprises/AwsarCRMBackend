0a149de4059cf877833da968f08b66363241e4d3
simran <simrankumaridga@gmail.com>
2024-08-06 17:01:24 +0530

    updated

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -48,7 +48,7 @@
             margin-top: 20px;
         }
         table {
-            width: 90%;
+            width: 98%;
             border-collapse: collapse;
             margin-bottom: 20px;
             border: 1px solid #ddd;
