8538d7caafb8b5f907dd4c608c578ba531b29c76
simran <simrankumaridga@gmail.com>
2024-08-07 16:40:22 +0530

    updated

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -92,8 +92,8 @@
         </div>
 
         <div class="logo">
-    @if (filter_var($details->logo, FILTER_VALIDATE_URL))
-        <img src="{{ $details->logo }}" alt="Company Logo" style="max-width: 200px; height: auto;">
+    @if ($details->logo)
+        <img src="https://awasarbackend.makkuenterprises.com/storage/uploads/xvrbUqJMcsV8cLBeHUaZD8YjQ9ABNf9COoZOOcBc.png" alt="Company Logo" style="max-width: 200px; height: auto;">
     @else
         <img src="{{ asset('storage/' . $details->logo) }}" alt="Company Logo" style="max-width: 200px; height: auto;">
     @endif
