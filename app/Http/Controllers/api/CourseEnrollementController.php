81ea3ef2a6bce6e65888006cccb9a4f97c335f6c
simran <simrankumaridga@gmail.com>
2024-08-10 11:59:12 +0530

    updated

--- app/Http/Controllers/api/CourseEnrollementController.php
+++ app/Http/Controllers/api/CourseEnrollementController.php
@@ -33,7 +33,7 @@ class CourseEnrollementController extends Controller
         'payment_type' => ['required', 'string', 'min:1', 'max:250'],
         'payment_status' => ['required', 'string', 'min:1', 'max:250'],
         'paid_amount' => ['required', 'numeric', 'min:0'],
-        'due_date' => 'nullable|date_format:d-m-y'
+        'due_date' => 'nullable'
     ]); 
 
     // Check if validation fails
