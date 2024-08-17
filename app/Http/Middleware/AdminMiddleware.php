cf05a8b11d9da567c9a191bcb5dde1aa40a0f603
simran <simrankumaridga@gmail.com>
2024-08-17 15:36:48 +0530

    updated

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -34,7 +34,7 @@ class AdminMiddleware
         return $next($request);
 
         }
-        return response()->json(['message' => 'Unauthorized, please log in as admin'], 401);
+        return response()->json(['message' => 'Unauthorized'], 401);
 
     } 
 }
