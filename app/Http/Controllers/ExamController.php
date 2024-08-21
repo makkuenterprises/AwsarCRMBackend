3bbd72b23c64c7bceada0e7ed2f5482892d6067c
simran <simrankumaridga@gmail.com>
2024-08-21 14:03:25 +0530

    updated

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -8,6 +8,7 @@ use App\Models\ExamQuestion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 use Carbon\Carbon;
 
@@ -220,7 +221,7 @@ public function listExamsForBatch($batchId)
             'error' => $e->getMessage()
         ], 500);
     }
-}
+} 
  
 public function listQuestionsForExam($examId) 
 {
