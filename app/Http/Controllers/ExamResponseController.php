0316fce7b7f03f3ef28a0408f17a4d80b5d7954c
simran <simrankumaridga@gmail.com>
2024-08-31 15:19:51 +0530

    updated

--- app/Http/Controllers/ExamResponseController.php
+++ app/Http/Controllers/ExamResponseController.php
@@ -1667,9 +1667,12 @@ public function getResponsesByBatchAndStudent(Request $request)
                             'question_options' => $questionOptions, 
                             'max_marks' => $examQuestion->marks,
                             'correct_answer' => $examQuestion->question->correct_answers, // Assuming a correct_answers field
-                          'student_response' => $studentResponse->response 
-                                ? array_map('strval', json_decode($studentResponse->response)) 
-                                : [],
+                        //   'student_response' => $studentResponse->response 
+                        //         ? array_map('strval', json_decode($studentResponse->response)) 
+                        //         : [],
+                        'student_response' => $studentResponse->response
+    ? array_map('strval', (array) json_decode($studentResponse->response, true))
+    : [],
                             'gained_marks' => $obtainedMarks,
                             'negative_marks' => $studentResponse->negative_marks ?? null,
                             // 'status' => $isCorrect ? 'correct' : 'wrong',
